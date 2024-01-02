671b651abcdf88a9b1e7f80cf4a00671027f43b2
surabhi-agnihotri <114336295+surabhi-agnihotri@users.noreply.github.com>
2024-01-02 21:06:59 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
-# Spotify Clone Project
+# Spotify-Clone
 
-Welcome to my Spotify Clone Project! This project is a simple clone of the Spotify application, allowing users to enjoy a curated playlist of my favorite songs.
+As part of learning to code,I completed a project that involved creating a basic Spotify clone using HTML, CSS, and JavaScript. This project was designed to showcase my skills in front-end web development.It is a responsive and user-friendly web application that allows users to play music.I used HTML and CSS to design and style the interface, and I implemented JavaScript to handle user input and interaction such as basic playback controls -play, pause, and next.I also gained experience working with version control tools such as Git and GitHub.
 
-## Overview
+## Tech-Stack-
 
-This project is a part of my web development journey. The Spotify Clone provides a streamlined interface where users can seamlessly listen to their favorite tunes without distractions.
+<div align="left">
+<img alt="HTML5" src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"/>
+<img alt="CSS3" src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"/> 
+<img alt="JavaScript" src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"/>
+</div>
 
-## Features
+## Features-
 
-- **Curated Playlist:** Enjoy a handpicked selection of my favorite songs.
+- Play/Pause: Allows the user to start or pause the playback of the currently selected track.
+- Next/Previous: Enables the user to skip to the next or previous track in the playlist or album.
+- Seek: Allows the user to manually move forward or backward within a track by dragging a slider or clicking on a progress bar.
+- Display the currently playing song and album artwork.
 
-- **Song Playback:** Play, pause, skip, and control the playback of songs within the application.
+## Usage-
+
+Once the Spotify clone is open in your browser, You can then click the play button next to the song to start playing it, and use the playback controls at the bottom of the page to control the song playback.The currently playing song and album artwork are displayed in the "Now Playing" section at the bottom of the page.
+
+## Live-Demo-
+
+[Spotify-Clone-Live](https://spotify-clone-javascript.netlify.app/)
 
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/e37fba97-0766-4626-9212-06a9fa3e5f00/deploy-status)](https://app.netlify.com/sites/animated-marshmallow-d90790/deploys)
 
 
+## Screenshot-
 
-## Getting Started
-
-1. **Clone the Repository:**
-
-   ```bash
-   git clone https://github.com/your-username/Spotify-Clone-Project.git
-   cd Spotify-Clone-Project
+![image](https://user-images.githubusercontent.com/48729682/222124168-70e510a9-0232-4c71-9939-cd7e8c512fcc.png)
---
+++ script.js
@@ -0,0 +1,107 @@
+// Initialize the Variables
+let songIndex = 0;
+let audioElement = new Audio('songs/1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
+let songs = [
+    {songName: "Tum Hi Ho - Arijit Singh", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Channa Mereya - Arijit Singh", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Gerua - Arijit Singh", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Raabta - Arijit Singh", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Tere Bina - A.R. Rahman", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Jeene Laga Hoon - Atif Aslam", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Ae Dil Hai Mushkil - Arijit Singh", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Tera Ban Jaunga - Akhil Sachdeva", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Zaalima - Arijit Singh", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Tum Jo Aaye - Rahat Fateh Ali Khan", filePath: "songs/10.mp3", coverPath: "covers/10.jpg"},
+]
+
+songItems.forEach((element, i)=>{ 
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
+ 
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+// Listen to Events
+audioElement.addEventListener('timeupdate', ()=>{ 
+    // Update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100); 
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
+
+const makeAllPlays = ()=>{
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+        element.classList.remove('fa-pause-circle');
+        element.classList.add('fa-play-circle');
+    })
+}
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+    element.addEventListener('click', (e)=>{ 
+        makeAllPlays();
+        songIndex = parseInt(e.target.id);
+        e.target.classList.remove('fa-play-circle');
+        e.target.classList.add('fa-pause-circle');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        masterSongName.innerText = songs[songIndex].songName;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        gif.style.opacity = 1;
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex = 0
+    }
+    else{
+        songIndex += 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex = 0
+    }
+    else{
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    masterSongName.innerText = songs[songIndex].songName;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
